74fd5c997bb482d5a8d2dc0c38a6cc72d175d3a5
NoName4Me <anich.evas@gmail.com>
2020-02-02 19:02:01 +0800

    feat: concurrent version done

--- README.md
+++ README.md
@@ -7,3 +7,33 @@ yarn
 # 本地开发
 yarn start
 ```
+
+[v1.basic](https://github.com/learn-by-do/build-your-react/tree/v1.basic) 版本的会阻塞主线程，当树比较大时，浏览器会处于假死状态，无法即时响应交互输入。因此我们需要将任务碎片化，当执行完一个碎片任务则释放线程，可以利用 `requestIdleCallback` 来实现（注意: React 已经不再使用这个 API，见 [issue#11171](https://github.com/facebook/react/issues/11171#issuecomment-417349573)，目前使用的是 [scheduler](https://github.com/facebook/react/tree/master/packages/scheduler)），这个 API 是当主线程空闲时激活回调函数。
+
+
+## 如何划分任务
+
+假设需要渲染这样一个树结构：
+
+```js
+React.render(
+  <div>
+    <h1>
+      <p />
+      <a />
+    </h1>
+    <h2 />
+  </div>,
+  container
+)
+```
+
+它对应的 fiber 树如下图，每一个 fiber 有三个链，分别指向父、第一个子、下一个兄弟。
+
+![](fiber.png)
+
+这种数据结构便于获取到下一个碎片任务。
+
+当一个 fiber 的任务完成时，如果它有子，那么这个子就会是下一个碎片任务（如上例中的 `div` 结束后，下一个就是 `h1`，如果没有子，那么它的兄弟会是下一个碎片任务（上例中 `p` 结束后，下一个就是 `a`）。
+
+体现在代码里：[performUnitOfWork](./src/render.js#L74)
--- src/render.js
+++ src/render.js
@@ -1,11 +1,5 @@
-/**
- * render virtual DOM to DOM
- * 
- * @param {*} element 
- * @param {*} container 
- */
-function render(element, container) {
-  const { type, props } = element;
+function createDom(fiber) {
+  const { type, props } = fiber;
   const dom =
     type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
@@ -18,10 +12,108 @@ function render(element, container) {
     .forEach(prop => {
       dom[prop] = props[prop];
     });
+  return dom;
+}
 
-  props.children.forEach(child => render(child, dom));
 
-  container.appendChild(dom);
+function commitRoot() {
+  commitWork(wipRoot.child)
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if(!fiber) {
+    return;
+  }
+
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+/**
+ * render virtual DOM to DOM
+ *
+ * @param {*} element
+ * @param {*} container
+ */
+function render(element, container) {
+  // 初始化第一个碎片任务
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  };
+  nextUnitOfWork = wipRoot;
+}
+
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  if(!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+/**
+ * perform work, return next unit work
+ *
+ * @param {*} fiber
+ */
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  // 这个会导致 UI 不完整渲染，所以拆解为 commitRoot，当没有碎片任务时，整体渲染 DOM
+  // if (fiber.parent) {
+  //   fiber.parent.dom.appendChild(fiber.dom);
+  // }
+
+  // create new fiber from children
+  const { children } = fiber.props;
+  let preSibling = null;
+
+  for (const child of children) {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      dom: null
+    };
+    // 设置子
+    if (!preSibling) {
+      fiber.child = newFiber;
+    } else {
+      // 设置兄弟
+      preSibling.sibling = newFiber;
+    }
+    preSibling = newFiber;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 export default render;
